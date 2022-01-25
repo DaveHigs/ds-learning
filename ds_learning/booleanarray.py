b4fbceab840a01ca237fd01315fdde16a0e849b9
DaveHigs <david.higuera1@hotmail.com>
2022-01-25 00:58:17 -0300

    initial commit

---
+++ ds_learning/booleanarray.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+a = np.full((2, 2, 2), True)
+
+print(a)
---
+++ testcsv.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+data = pd.read_csv('data/Hotel_Reviews.csv')
+
+data.to_parquet('data/Hotel_Reviews.parquet')
